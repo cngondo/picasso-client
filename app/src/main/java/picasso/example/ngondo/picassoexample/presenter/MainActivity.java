42edaf2a86c9936b6520bea631555b31bb806fae
cngondo <devngondo@gmail.com>
2016-06-17 11:36:41 +0300

    Packages and views

--- app/src/main/java/picasso/example/ngondo/picassoexample/presenter/MainActivity.java
+++ app/src/main/java/picasso/example/ngondo/picassoexample/presenter/MainActivity.java
@@ -1,4 +1,4 @@
-package picasso.example.ngondo.picassoexample;
+package picasso.example.ngondo.picassoexample.presenter;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import picasso.example.ngondo.picassoexample.R;
 
 public class MainActivity extends AppCompatActivity {
 
