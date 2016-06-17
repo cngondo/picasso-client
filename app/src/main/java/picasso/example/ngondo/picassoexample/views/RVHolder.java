85d3d9d06ec782fe661eb5191001da3dba1272fc
cngondo <devngondo@gmail.com>
2016-06-17 11:48:41 +0300

    Holder for the views

--- app/src/main/java/picasso/example/ngondo/picassoexample/views/RVHolder.java
+++ app/src/main/java/picasso/example/ngondo/picassoexample/views/RVHolder.java
@@ -1,7 +1,24 @@
 package picasso.example.ngondo.picassoexample.views;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import picasso.example.ngondo.picassoexample.R;
+
 /**
  * Created by ngondo on 6/17/16.
  */
-public class RVHolder {
+public class RVHolder extends RecyclerView.ViewHolder{
+
+    TextView mName;
+    ImageView mImage;
+
+    public RVHolder(View itemView) {
+        super(itemView);
+
+        mName = (TextView) itemView.findViewById(R.id.movieName);
+        mImage = (ImageView) itemView.findViewById(R.id.movieImage);
+    }
 }
